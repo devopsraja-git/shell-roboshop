fcd49aa34a24b411c02aeac387daa62c794f8854
Raja Shekar <devops86sraja@gmail.com>
2025-09-23 17:39:01 +0530

    new updates

--- roboshop.sh
+++ roboshop.sh
@@ -5,7 +5,7 @@ SG_ID="sg-043a4d7e2b43cb151"
 
 for instance in $@
 do
-    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --count 1 --instance-type t3.micro --security-group-ids sg-043a4d7e2b43cb151 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=Test}]" --query 'Instances[0].InstanceId' --output text)
+    INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --count 1 --instance-type t3.micro --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=Test}]" --query 'Instances[0].InstanceId' --output text)
 
     #GET PRIVAE IP
 
