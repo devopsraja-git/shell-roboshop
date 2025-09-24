0811d207bef5672042844a80e3354add93c009c7
Raja Shekar <devops86sraja@gmail.com>
2025-09-24 14:29:05 +0530

    new updates

--- catalogue.sh
+++ catalogue.sh
@@ -75,7 +75,7 @@ validate $? "Enabling Catalogue service.."
 systemctl start catalogue
 validate $? "Starting Catalogue service.."
 
-cp mongo.repo /etc/yum.repos.d/mongo.repo
+cp $SCRIPT_DIR/mongo.repo /etc/yum.repos.d/mongo.repo
 validate $? "Creating mongo repo"
 
 dnf install mongodb-mongosh -y &>>$LOG_FILE
