24a7dd3aa579041b3e6c2272dbf7dacced7b09d1
Raja Shekar <devops86sraja@gmail.com>
2025-09-24 14:10:53 +0530

    new updates

---
+++ catalogue.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+uid=$(id -u)
+
+
+LOGS_FOLDER="/var/log/shell-roboshop"
+SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+MONGODB_HOST=mongodb.devraxtech.fun
+
+mkdir -p $LOGS_FOLDER
+echo "Script started executed at $(date)"
+
+if [ $uid -ne 0 ]; then
+    echo -e "ERROR:: Please run this as a $G ROOT $N User Privileges only"
+    exit 1
+fi
+
+validate(){
+    if [ $1 -ne 0 ]; then
+    echo -e "$2 is $R FAILED $N"
+    exit 1
+else
+    echo -e "$2 is $G SUCCESSFUL $N"
+fi
+}
+
+
+dnf module disable nodejs -y &>>$LOG_FILE
+validate $? "nodejs disabled"
+
+dnf module enable nodejs:20 -y
+validate $? "Enabled nodejs 20v"
+
+dnf install nodejs -y
+validate $? "Installing nodejs.."
+
+id roboshop
+    if ( $? -ne 0 ); then
+        useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop
+        validate $? "User roboshop created"
+    else
+        echo -e "User roboshop already exists...$Y SKIPPING.. $N"
+    fi
+
+mkdir -p /app
+validate $? "Creating app directory.."
+
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip 
+validate $? "Downloading catalogue application.."
+cd /app 
+validate $? "Changing to app directory.."
+rm -rf /app/*
+unzip /tmp/catalogue.zip
+validate $? "Unzipping/Extracting the app code.."
+
+
+npm install 
+validate $? "Installing dependencies.."
+
+cp catalogue.service /etc/systemd/system/catalogue.service
+validate $? "Copying catalogue services.."
+
+systemctl daemon-reload
+
+systemctl enable catalogue 
+validate $? "Enabling Catalogue service.."
+systemctl start catalogue
+validate $? "Starting Catalogue service.."
+
+cp mongo.repo /etc/yum.repos.d/mongo.repo
+validate $? "Creating mongo repo"
+
+dnf install mongodb-mongosh -y
+validate $? "Installing mongodb client.."
+
+mongosh --host $MONGODB_HOST </app/db/master-data.js
+validate $? "Loading mongodb data ..."
