552bc31a2ad6581959b420b26ab02b8d1e3a10ca
Raja Shekar <devops86sraja@gmail.com>
2025-09-25 15:37:34 +0530

    2509updates

--- redis.sh
+++ redis.sh
@@ -41,8 +41,8 @@ validate $? "Enabling redis version 7.."
 dnf install redis -y &>>$LOG_FILE
 validate $? "Installing redis.."
 
-sed -i 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/ c protected-mode no' /etc/redis/redis.conf /etc/redis/redis.conf
-validate $? "Updating global IP for redis.."
+sed -i 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/ c protected-mode no' /etc/redis/redis.conf
+validate $? "Updating global IP  and Protect mode for redis.."
 
 systemctl enable redis &>>$LOG_FILE
 validate $? "Enabling redis service.."
